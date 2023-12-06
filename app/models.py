1027ea1f9657de4802bcefc177998b3224273f69
unknown <Anandh154>
2023-12-06 21:08:10 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class ProductData(models.Model):
+    category=models.CharField(max_length=100)
+    def __str__(self):
+        return self.category
+class Product(models.Model):
+    pid=models.IntegerField()
+    product=models.CharField(max_length=100)
+    date=models.DateField()
+    price=models.DecimalField(max_digits=6,decimal_places=3)
+    category=models.ForeignKey(ProductData,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product
